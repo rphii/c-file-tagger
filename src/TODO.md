c5f619e62357fadb81850e7bc775b793aefe1304
rphii <raphael.seitz18@gmail.com>
2025-01-19 18:41:23 +0100

    idk

---
+++ src/TODO.md
@@ -0,0 +1,2 @@
+WHY DID IT GET 4x SLOWER from commit d6f5e04 until main?
+
--- src/prof.sh
+++ src/prof.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-execute="cft"
+execute="cft.out"
 cmd="./$execute $@ && gprof ./$execute gmon.out > prof_$execute.txt && rm gmon.out && vim prof_$execute.txt"
 echo $cmd
 eval $cmd
